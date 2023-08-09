8ba55c47d37d7b806a3bd093b50d0ccef4c9e929
Michael Almeida <michael.almeida@fundatec.org.br>
2023-08-08 22:19:44 -0300

    organo finalizado

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <>
+      <header className="banner">
+        <img src="/imagens/banner.png" alt="banner principal"></img>
+      </header>
+    </>
+  );
+}
+export default Banner;
